e80a98628d48cf483cfde1d6d4a1512eb5c8a6e0
Louis Paul Romero <louispaulromero@gmail.com>
2018-01-02 01:03:52 -0800

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function initMap() {
-  var california = {lat: 36.674544, lng: -121.666438};
+  var california = {lat: 36.674567, lng: -121.666430};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 20,
     center: california,
     mapTypeId: 'satellite',
     disableDefaultUI: true
   });
+  map.setTitle(45);
 }
